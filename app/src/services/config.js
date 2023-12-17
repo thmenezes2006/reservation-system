b15ddcccbefc3cbf8103ae44f1a3c285720dcedb
Thiago Menezes <107003617+thmenezes2006@users.noreply.github.com>
2023-12-16 22:31:25 -0300

    try again config api vercel

--- app/src/services/config.js
+++ app/src/services/config.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
-const api = `${process.env.VUE_APP_IP_API}` + `:${process.env.VUE_APP_PORT_REST}`
+let api = `${process.env.VUE_APP_IP_API}`
+
+if (process.env.VUE_APP_PORT_REST !== '') {
+    api = api + `:${process.env.VUE_APP_PORT_REST}`
+}
+
 export const http = axios.create({
     baseURL: api
 })
