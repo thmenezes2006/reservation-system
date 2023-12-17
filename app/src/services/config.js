a649dfdb8ba3264518acea278fae7ebb91c564ae
Thiago Menezes <107003617+thmenezes2006@users.noreply.github.com>
2023-12-16 23:08:20 -0300

    remove logs

--- app/src/services/config.js
+++ app/src/services/config.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 let api = `${process.env.VUE_APP_IP_API}`
-console.log('apiAntes', api);
+
 if (process.env.VUE_APP_PORT_REST) {
     api = api + `:${process.env.VUE_APP_PORT_REST}`
 }
-console.log('apiDepois', api);
+
 export const http = axios.create({
     baseURL: api
 })
